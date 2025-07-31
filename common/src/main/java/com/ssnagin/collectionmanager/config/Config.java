e26a49d0a3f49ecfcbb2d7d7a684352763581b0a
ssnagin <56646530+ssnagin@users.noreply.github.com>
2025-07-31 21:18:41 +0300

    1.3.0 Milestone

--- common/src/main/java/com/ssnagin/collectionmanager/config/Config.java
+++ common/src/main/java/com/ssnagin/collectionmanager/config/Config.java
@@ -4,7 +4,7 @@ public class Config {
     public class Core {
 
 
-        public static String VERSION = "1.1.0";
+        public static String VERSION = "1.3.0";
 
 
         public static final int RESPONSE_SENDER_PARALLELISM = 8;
